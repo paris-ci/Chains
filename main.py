0e6f8dc0bbb9ff81f7323e8714472a64144dec30
paris-ci <jovart.arthur@gmail.com>
2018-08-18 15:47:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,137 @@
+import time
+import typing
+
+import markovify
+import requests
+from tqdm import tqdm
+
+
+class BaseCorpus:
+    def __init__(self):
+        pass
+
+    @property
+    def model(self) -> typing.Union[markovify.NewlineText, markovify.Text]:
+        # In subclasses, you'd have to do something so that we can use the model with self.model
+        return None
+
+    @model.setter
+    def model(self, value):
+        self._model = value
+
+    @model.deleter
+    def model(self):
+        pass
+
+    def make_sentence(self, max_words: int = None):
+        s = self.model.make_sentence(max_words=max_words)
+
+        return s
+
+    def make_short_sentence(self, chars: int = 140, more_or_less: int = 20):
+        s = self.model.make_short_sentence(max_chars=chars + more_or_less, min_chars=chars - more_or_less)
+
+        return s
+
+    def make_sentence_with_start(self, start: str):
+        s = self.model.make_sentence_with_start(start.lower(), strict=False)
+
+        return s
+
+    def save(self, file_name):
+        with open(file_name, 'w') as file:
+            file.write(self.model.to_json())
+
+
+class DuckHuntCorpus(BaseCorpus):
+    def __init__(self):
+        super().__init__()
+        self.url = 'http://duckhunt.api-d.com/messages.txt'
+        self._model = None
+
+    @property
+    def model(self):
+        if self._model is None:
+            self._model = self.create_markov(self.process_corpus(self.download()))
+
+        return self._model
+
+    @model.setter
+    def model(self, value):
+        self._model = value
+
+    @model.deleter
+    def model(self):
+        if self._model:
+            self._model = None
+
+    def update(self):
+        del self.model
+
+    def download(self) -> requests.Response:
+        """
+        Download the corpus with a progress bar shown to the user
+        :return:
+        """
+        # Streaming, so we can iterate over the response.
+        r = requests.get(self.url, stream=True)
+
+        return r
+
+    def process_corpus(self, file: requests.Response) -> str:
+
+        start_length = 0
+        start_time = time.time()
+
+        lines_parsed = []
+
+        for line in tqdm(file.iter_lines(), desc="Downloading and processing text corpus"):
+            start_length += 1
+            line = line.decode()
+            message = line.lower().strip()
+
+            if len(message) < 2:
+                continue
+
+            if message.startswith('dh'):
+                continue
+
+            if any([x in message for x in ['nigga', 'http', 'discord.gg', 'fuck', 'hentai']]):
+                continue
+
+            for word in message.split(" "):
+                if len(word) > 15:
+                    break
+
+
+
+            else:
+                lines_parsed.append(message)
+
+        lines_parsed = sorted(list(set(lines_parsed)))
+
+        end_length = len(lines_parsed)
+        end_time = time.time()
+
+        print(f"Processing the corpus took {round(end_time-start_time, 3)}s, and the corpus was trimmed from {start_length} to {end_length} lines (diff: {end_length-start_length})")
+
+        return "\n".join(lines_parsed)
+
+    def create_markov(self, text: str) -> markovify.NewlineText:
+        return markovify.NewlineText(text, state_size=4, retain_original=False)
+
+    def load(self, file_name):
+        start_time = time.time()
+        with open(file_name, 'r') as file:
+            model = markovify.NewlineText.from_json(file.read())
+
+        end_time = time.time()
+        print(f'Recovered model from file in {round(end_time-start_time, 3)}s')
+        self.model = model
+
+
+c = DuckHuntCorpus()
+c.save('model.json')
+
+while True:
+    print(c.make_sentence_with_start(input('>')))
